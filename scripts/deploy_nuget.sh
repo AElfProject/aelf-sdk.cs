b5029a2cb077612a53e2dcb5275d38b132f66cfa
peng <rongpeng@live.com>
2019-12-07 14:19:59 +0800

    Update deploy to nuget scripts

--- scripts/deploy_nuget.sh
+++ scripts/deploy_nuget.sh
@@ -1,27 +1,20 @@
 #!/usr/bin/env bash
-set -ev
 
 TAG=$1
 NUGET_API_KEY=$2
 VERSION=`echo ${TAG} | cut -b 2-`
 
-src_path=./AElf.Client
-build_output=/tmp/aelf-build
+build_output=/tmp/aelf-sdk
 
 if [[ -d ${build_output} ]]; then
     rm -rf ${build_output}
 fi
 
 dotnet restore AElf.Client.sln
+dotnet build AElf.Client/AElf.Client.csproj /clp:ErrorsOnly --configuration Release -P:Version=${VERSION} -P:Authors=AElf -o ${build_output}
 
-echo '---- build '${src_path}
+echo `ls ${build_output}/AElf.Client.${VERSION}.nupkg`
+dotnet nuget push ${build_output}/AElf.Client.${VERSION}.nupkg -k ${NUGET_API_KEY} -s https://api.nuget.org/v3/index.json
 
-name=AElf.Client
-do
-    echo ${name}/${name}.csproj
-    dotnet build ${name}/${name}.csproj /clp:ErrorsOnly --configuration Release -P:Version=${VERSION} -P:Authors=AElf -o ${build_output}
+echo 'done'
 
-    echo `ls ${build_output}/${name}.${VERSION}.nupkg`
-    dotnet nuget push ${build_output}/${name}.${VERSION}.nupkg -k ${NUGET_API_KEY} -s https://api.nuget.org/v3/index.json
-done
-cd ../
